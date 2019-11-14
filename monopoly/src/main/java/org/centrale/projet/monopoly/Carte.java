2a2821c226c7ec4a845e691f41a950dbe5f01bc6
Eloi159 <57747465+Eloi159@users.noreply.github.com>
2019-11-14 11:42:17 +0100

    Add files via upload
    Modification de Carte

--- monopoly/src/main/java/org/centrale/projet/monopoly/Carte.java
+++ monopoly/src/main/java/org/centrale/projet/monopoly/Carte.java
@@ -1,5 +1,11 @@
-package org.centrale.projet.monopoly;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.centralenantes.projetmedevei2;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -9,16 +15,38 @@ import java.util.Random;
 public class Carte extends NonAchetable{
     private String type;
 
-    public Carte(String type) {
+    public Carte(String nom, String type) {
+        super(nom);
         this.type = type;
     }
     
     public Carte(Carte c){
+        super(c.getNom());
         this.type = c.type;
     }
     
     public Carte(){
-        this.type = "Départ";
+        super("Chance");
+        Random generateurAleatoire = new Random();
+        int n = generateurAleatoire.nextInt(3);
+        switch(n){
+            case 0:{
+                this.type = "Départ";
+                break;
+            }
+            case 1:{
+                this.type = "Prison";
+                break;
+            }
+            case 2:{
+                this.type = "Gain";
+                break;
+            }
+            case 3:{
+                this.type = "Perte";
+                break;
+            }
+        }
     }
 
     public String getType() {
@@ -32,26 +60,28 @@ public class Carte extends NonAchetable{
     public void action(Joueur j){
         switch(type){
             case "Départ":{
-                j.setPosition(new Depart());
+                j.setCase(new Case("Départ"));
                 break;
             }
             case "Prison":{
-                j.setPosition(new Prison());
+                j.setCase(new Case("Prison"));
                 break;
             }
             case "Gain":{
                 Random generateurAleatoire = new Random();
                 int x = generateurAleatoire.nextInt(15000);
-                j.setArgent(j.getArgent()+5000+x);
+                j.setArgent(Joueur.getArgent()+5000+x);
                 break;
             }
             case "Perte":{
                 Random generateurAleatoire = new Random();
                 int x = generateurAleatoire.nextInt(14000);
-                j.setArgent(j.getArgent()-(1000+x));
+                j.setArgent(Joueur.getArgent()-(1000+x));
                 break;
             }
         }
     }
+    
+    
     
 }
