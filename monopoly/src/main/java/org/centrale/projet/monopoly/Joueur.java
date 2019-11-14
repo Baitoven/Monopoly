7c349f19226e47d0fb2b446f9c541ace138e4e55
BlueCloud02 <comptes.98@gmail.com>
2019-11-14 10:06:18 +0100

    prepare merging

--- monopoly/src/main/java/org/centrale/projet/monopoly/Joueur.java
+++ monopoly/src/main/java/org/centrale/projet/monopoly/Joueur.java
@@ -1,102 +1,92 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.centrale.projet.monopoly;
 
 import org.centrale.projet.monopoly.monopoly.master.NoMoreMoney;
 
-/**
- *
- * @author Julien
- */
 public class Joueur {
+
     private Case position;
     private Plateau refPlateau;
     private int fortune;
     private String nom;
     private LinkedList<Case> possession = new LinkedList<>();
-    
-    public Joueur(String nom, Plateau refPlateau){
+
+    public Joueur(String nom, Plateau refPlateau) {
         this.nom = new String(nom);
         fortune = 1000000;
         this.refPlateau = refPlateau;
         nbGare = 0;
     }
-    
-    public Case getPosition(){
+
+    public Case getPosition() {
         return position;
     }
-    
-    public void setPosition(Case position){
+
+    public void setPosition(Case position) {
         this.position = position;
     }
-    
-     public Plateau getRefPlateau(){
+
+    public Plateau getRefPlateau() {
         return refPlateau;
     }
-    
-    public void setRefPlateau(Plateau refPlateau){
+
+    public void setRefPlateau(Plateau refPlateau) {
         this.refPlateau = refPlateau;
     }
-    
-    public int getFortune(){
+
+    public int getFortune() {
         return fortune;
     }
-    
-    public void setFortune(int value){
+
+    public void setFortune(int value) {
         fortune = value;
     }
-    
-    public String getNom(){
-        return(nom);
+
+    public String getNom() {
+        return (nom);
     }
-    
-    public void setNom(String value){
+
+    public void setNom(String value) {
         nom = new String(value);
     }
-    
-    public int nbGare(){
+
+    public int nbGare() {
         int compteur = 0;
-        for(int i =0; i<possession.size();i++){
-            if ((possession.get(i)) instanceof Gare)
+        for (int i = 0; i < possession.size(); i++) {
+            if ((possession.get(i)) instanceof Gare) {
                 compteur++;
+            }
         }
-        return(compteur)
+        return (compteur)
     }
-    
-    public void gagneArgent(int value){
+
+    public void gagneArgent(int value) {
         fortune = fortune + value;
     }
-    
-    public void perdArgent(int value){
+
+    public void perdArgent(int value) {
         fortune = fortune + value;
     }
-    
-    public void paiement(Joueur j, int paye) throws NoMoreMoney{
-        if (this.fortune >= paye){
+
+    public void paiement(Joueur j, int paye) throws NoMoreMoney {
+        if (this.fortune >= paye) {
             j.gagneArgent(paye);
             this.perdArgent(paye);
-        }
-        else {
+        } else {
             throw new NoMoreMoney("Plus d'argent");
         }
     }
-    
-    public void acheter(Achetable c) throws NoMoreMoney{
-        if (this.fortune >= c.getPrix()){
-           this.perdArgent(c.getPrix());
-	c.setProprietaire(nom);
-        possession.add((Case)c);
-        }
-        else {
+
+    public void acheter(Achetable c) throws NoMoreMoney {
+        if (this.fortune >= c.getPrix()) {
+            this.perdArgent(c.getPrix());
+            c.setProprietaire(nom);
+            possession.add((Case) c);
+        } else {
             throw new NoMoreMoney("Plus d'argent pour ça");
         }
     }
-    
-    public void vendre(Achetable c){
+
+    public void vendre(Achetable c) {
         this.gagneArgent(c.getPrix());
         possession.remove(c);
     }
--- monopoly/src/main/java/org/centrale/projet/monopoly/NoMoreMoney.java
+++ monopoly/src/main/java/org/centrale/projet/monopoly/NoMoreMoney.java
@@ -1,19 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package org.centrale.projet.monopoly.monopoly.master;
+package org.centrale.projet.monopoly;
 
 /**
  *
  * @author Pm
  */
 public class NoMoreMoney extends Exception {
-    
-    public NoMoreMoney (String s){
+
+    public NoMoreMoney(String s) {
         super(s);
-        
     }
 }
-
