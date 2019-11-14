fb664cd1e9771f82d7501604101eaa3fe30ceb73
Scae1842 <57747876+Scae1842@users.noreply.github.com>
2019-11-14 09:21:13 +0100

    Création classe d'exception NoMoreMoney

---
+++ monopoly/src/main/java/org/centrale/projet/monopoly/NoMoreMoney.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tpmedev1;
+
+/**
+ *
+ * @author PM
+ */
+public class NoMoreMoney extends Exception {
+    
+    public NoMoreMoney (String s){
+        super(s);
+        
+    }
+    
+}
