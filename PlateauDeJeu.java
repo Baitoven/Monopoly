e2c1d887a514c4820a46959b845d1042f942e54b
gheritarish <ilissiae@gmail.com>
2019-11-14 08:55:09 +0100

    Création de la classe Plateau

---
+++ PlateauDeJeu.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fabra
+ */
+public class PlateauDeJeu {
+    
+    private ArrayList<Case> plateau ;
+    
+    public PlateauDeJeu(ArrayList<Case)
+    
+    public boolean findePartie() {
+        
+    }
+}
