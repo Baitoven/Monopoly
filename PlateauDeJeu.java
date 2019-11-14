29e9c4e3fe4cbb7e88499babc6142614981e5d1d
gheritarish <ilissiae@gmail.com>
2019-11-14 10:44:16 +0100

    PlateauDeJeu : méthodes affichePlateau(), finDePartie(), avance(), afficheJoueur(), tourDeJeuComplet()

--- PlateauDeJeu.java
+++ PlateauDeJeu.java
@@ -14,7 +14,58 @@ public class PlateauDeJeu {
     
     public PlateauDeJeu(ArrayList<Case)
     
-    public boolean findePartie() {
-        
+   
+     public Case avance(int d, Joueur j) {
+        int index = (this.plateau.indexOf(j.getPosition()) + d) % 40;
+        return this.plateau.get(index);
+    }
+
+    public void affichePlateau() {
+        for (Case c : this.plateau) {
+            System.out.println(c.toString());
+        }
+    }
+
+    public boolean finDePartie() {
+        boolean r = true;
+        if(this.jouers.size()>1){
+            r = false;
+        }
+        return r;
+    }
+
+    public void afficheJoueur() {
+        String joueurString;
+        for (joueur j : joueurs) {
+            joueurString = j.nom + " " + Integer.toString(j.argent) + " euros\n" + "Cases: \n";
+
+            for (Case c : this.plateau) {
+                if (c instanceof Achetable) { // On parcourt le plateau pour trouver les cases qui apartient au joueur j
+                    if ((Achetable) c.proprietaire.equals(j)) {
+                        joueurString += c.toString + "\n";
+                    }
+                }
+            }
+            System.out.println(joueurString);
+        }
+    }
+    
+    public void tourDeJeuComplet(){
+        boolean fin = finDePartie();
+        int nbTour = 0;
+        while (!fin){
+            nbTour = nbTour + 1;
+            Iterator<Joueur> itJoueur = this.joueurs.iterator();
+            while (itJoueur.hasNext()){
+                Joueur j = itJoueur.next();
+                try {
+                    j.tourDeJeu();
+                }
+                catch (NoMoreMoney m){
+                    itJoueur.remove();
+                }
+            }
+            fin = finDePartie();
+        }
     }
 }
