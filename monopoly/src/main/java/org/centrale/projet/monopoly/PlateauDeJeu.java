31fb252dac6c9af03c0b74619c50ea65a7ede59e
João Pedro Alves de Abreu <joaopedro.alves.abreu@gmail.com>
2019-11-14 10:10:54 +0100

    Update PlateauDeJeu.java
    Méthodes ajoutées:
    - avance()
    - affichePlateau
    - finDePartie()
    - afficheJoueur()

--- monopoly/src/main/java/org/centrale/projet/monopoly/PlateauDeJeu.java
+++ monopoly/src/main/java/org/centrale/projet/monopoly/PlateauDeJeu.java
@@ -10,5 +10,40 @@ package org.centrale.projet.monopoly;
  * @author Céline Balmes
  */
 public class PlateauDeJeu {
+
+    public Case avance(int d, Joueur j) {
+        int index = (this.plateau.indexOf(j.getPosition()) + d) % 40;
+        return this.plateau.get(index);
+    }
+
+    public void affichePlateau() {
+        for (Case c : this.plateau) {
+            System.out.println(c.toString());
+        }
+    }
+
+    public boolean finDePartie() {
+        boolean r = true;
+        if(this.jouers.size()>1){
+            r = false;
+        }
+        return r;
+    }
+
+    public void afficheJoueur() {
+        String joueurString;
+        for (joueur j : joueurs) {
+            joueurString = j.nom + " " + Integer.toString(j.argent) + " euros\n" + "Cases: \n";
+
+            for (Case c : this.plateau) {
+                if (c instanceof Achetable) { // On parcourt le plateau pour trouver les cases qui apartient au joueur j
+                    if ((Achetable) c.proprietaire.equals(j)) {
+                        joueurString += c.toString + "\n";
+                    }
+                }
+            }
+            System.out.println(joueurString);
+        }
+    }
     
 }
