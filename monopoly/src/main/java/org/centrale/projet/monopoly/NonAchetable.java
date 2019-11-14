b038a35a7abce50c939b8f8f9fe43fb8eea34c27
BlueCloud02 <comptes.98@gmail.com>
2019-11-14 12:12:05 +0100

    debugging in project

--- monopoly/src/main/java/org/centrale/projet/monopoly/NonAchetable.java
+++ monopoly/src/main/java/org/centrale/projet/monopoly/NonAchetable.java
@@ -5,6 +5,17 @@ package org.centrale.projet.monopoly;
  * @author louis-alexandre
  */
 public abstract class NonAchetable extends Case{
+    public NonAchetable(String nom){
+        super(nom);
+    }
+    
+    public NonAchetable(NonAchetable a){
+        super(a.getNom());
+    }
+    
+    public NonAchetable(){
+        super();
+    }
     
    @Override
     public String toString(){
